c5b3a5c17d981043393fa24128403f4c33b6aaa6
totlu1 <totluca@gmail.com>
2015-05-25 09:25:37 +0200

    Create sudoku.hpp

---
+++ sudoku.hpp
@@ -0,0 +1,43 @@
+#ifndef SODOKU_HPP_INCLUDED
+#define SODOKU_HPP_INCLUDED
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include "alap.hpp"
+#include "skocka.hpp"
+
+
+using namespace std;
+using namespace genv;
+
+class sodoku
+{
+protected:
+int x,y,sx,sy,r,g,b;
+
+
+public:
+
+bool kattintas;
+char szam;
+int x0, y0, kival;
+event ev;
+vector<ALAP*>a;
+
+
+sodoku();
+sodoku(int _x, int _y, int _sx, int _sy);
+virtual void rajzolas();
+     virtual void viselkedes();
+bool kivalasztas (int x0, int y0)const;
+void beolvasas();
+
+
+};
+
+
+#endif // SODOKU_HPP_INCLUDED
