9326661cb81891250f73da3341f79b3200c74d10
totlu1 <totluca@gmail.com>
2015-05-25 09:21:47 +0200

    Create alap.hpp
    őswidget

---
+++ alap.hpp
@@ -0,0 +1,37 @@
+#ifndef ALAP_HPP_INCLUDED
+#define ALAP_HPP_INCLUDED
+#include "graphics.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+using namespace genv;
+
+class ALAP
+{
+protected:
+int x,y,sx,sy,r,g,b,skalamax, skalamin;
+
+public:
+
+    ALAP();
+    ALAP(int _x, int _y, int _sx, int _sy, int _r, int _g, int _b);
+     ALAP(int _x, int _y, int _sx, int _sy, int _r, int _g, int _b, int _skalamin, int _skalamax);
+
+
+virtual void rajzolas();
+virtual void viselkedes(int &x0, int &y0, event ev);
+virtual bool kivalasztas(int x0, int y0)const;
+
+
+
+};
+
+
+
+#endif // ALAP_HPP_INCLUDED
